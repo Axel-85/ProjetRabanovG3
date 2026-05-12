e19c89d48874a243f8c76748269561b74550adf0
abigaud <axelbigaud72@gmail.com>
2026-05-12 10:25:52 +0200

    blabla

--- ProjetRabanovG3/HotelData.cs
+++ ProjetRabanovG3/HotelData.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ProjetRabanovG3
-{
-    public class HotelData
-    {
-       using Microsoft.Ajax.Utilities;
+﻿using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,72 +9,76 @@ using MySqlConnector;
 using System.Configuration;
 using System.Data;
 
-namespace ProjetRabanovG3
+    public class HotelData
+{ }
+
+        namespace ProjetRabanovG3
+        {   
+    internal List<Client> ChargerClients()
     {
-        internal List<Client> ChargerClients()
+        List<Client> clients = new List<Client>();
+
+        string connectionString = "Server=192.168.37.155;Database=hotel";
+
+        using (MySqlConnection connexion = new MySqlConnection(connectionString))
         {
-            List<Client> clients = new List<Client>();
+            connexion.Open();
 
-            string connectionString = "Server=192.168.37.155;Database=###;User ID=hotel_user;Password=###;";
-
-            using (MySqlConnection connexion = new MySqlConnection(connectionString))
+            using (MySqlCommand cmd = new MySqlCommand())
             {
-                connexion.Open();
+                cmd.Connection = connexion;
+                cmd.CommandText = "SELECT id, nom, prenom FROM client";
 
-                using (MySqlCommand cmd = new MySqlCommand())
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
                 {
-                    cmd.Connection = connexion;
-                    cmd.CommandText = "SELECT id, nom, prenom FROM client";
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
 
-                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    foreach (DataRow row in table.Rows)
                     {
-                        DataTable table = new DataTable();
-                        adapter.Fill(table);
+                        Client client = new Client(Convert.ToInt32(row["id"]), row["nom"].ToString(), row["prenom"].ToString());
 
-                        foreach (DataRow row in table.Rows)
-                        {
-                            Client client = new Client(Convert.ToInt32(row["id"]), row["nom"].ToString(), row["prenom"].ToString());
-
-                            clients.Add(client);
-                        }
+                        clients.Add(client);
                     }
                 }
             }
-
-            return clients;
         }
 
+        return clients;
+    }
 
-        internal List<Sejour> ChargerSejours(int clientId)
+
+    internal List<Sejour> ChargerSejours(int clientId)
+    {
+        List<Sejour> sejours = new List<Sejour>();
+
+        string connectionString = "Server=192.168.37.155;Database=###;User ID=###;Password=###;";
+
+        using (MySqlConnection connexion = new MySqlConnection(connectionString))
         {
-            List<Sejour> sejours = new List<Sejour>();
-
-            string connectionString = "Server=192.168.37.155;Database=###;User ID=###;Password=###;";
-
-            using (MySqlConnection connexion = new MySqlConnection(connectionString))
+            using (MySqlCommand cmd = new MySqlCommand())
             {
-                using (MySqlCommand cmd = new MySqlCommand())
+                cmd.Connection = connexion;
+                cmd.CommandText = $"SELECT date_debut, date_fin FROM sejour WHERE client_id = {clientId}";
+                // cmd.Parameters.AddWithValue("@clientId", clientId);
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
                 {
-                    cmd.Connection = connexion;
-                    cmd.CommandText = $"SELECT date_debut, date_fin FROM sejour WHERE client_id = {clientId}";
-                    // cmd.Parameters.AddWithValue("@clientId", clientId);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
 
-                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    foreach (DataRow row in table.Rows)
                     {
-                        DataTable table = new DataTable();
-                        adapter.Fill(table);
+                        Sejour sejour = new Sejour(Convert.ToDateTime(row["date_debut"]), Convert.ToDateTime(row["date_fin"]));
 
-                        foreach (DataRow row in table.Rows)
-                        {
-                            Sejour sejour = new Sejour(Convert.ToDateTime(row["date_debut"]), Convert.ToDateTime(row["date_fin"]));
-
-                            //test
-                            sejours.Add(sejour);
-                        }
+                        //test
+                        sejours.Add(sejour);
                     }
                 }
             }
-
-            return sejours;
         }
-}
+
+        return sejours;
+
+        }
+    }
---
+++ ProjetRabanovG3/WebUserControl1.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="WebUserControl1.ascx.cs" Inherits="ProjetRabanovG3.WebUserControl1" %>
