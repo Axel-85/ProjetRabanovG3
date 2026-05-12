333bb0a0aa5f377949b9b318daf2fa6ccff9b03f
Axel-85 <axelbigaud72@gmail.com>
2026-05-12 15:06:52 +0200

    Add files via upload

---
+++ GestionFidelite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetRabanovG3
+{
+    public class GestionFidelite
+    {
+        public static int CalculerPoints(Client client)
+        {
+            client.PointsFidelite = client.Sejours.Count * 20;
+        }
+        public static StatutFidelite DeterminerStatut(int points)
+        {
+            if(points < 200)
+            {
+                return new StatutBronze();
+            }
+            else if (points < 500)
+            {
+                return new StatutArgent();
+            }
+            else
+            {
+                return new StatutOr();
+            }
+        }
+    }
+}
